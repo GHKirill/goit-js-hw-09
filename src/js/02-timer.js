bfb5e0fad01bb2d5e9cc8fc20744982e43a7044b
Kirill Voronin <voroninkv09@gmail.com>
2023-02-15 13:07:21 +0200

    change timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -86,5 +86,5 @@ ref.buttonStart.addEventListener('click', showCountDownPeriodOfTime);
 
 function showCountDownPeriodOfTime(event) {
   const timerID = setInterval(periodOfTimeCalculation, 1000);
-  ref.buttonStart.removeEventListener('click', showCountDownPeriodOfTime);
+  //ref.buttonStart.removeEventListener('click', showCountDownPeriodOfTime);
 }
